fe3843f95ad9780428c4bb7c4b544d66746a5f21
khaled nassar <58118235+khalednassar500@users.noreply.github.com>
2023-04-23 22:28:01 +0200

    Update TypingArea.js

--- src/components/TypingArea.js
+++ src/components/TypingArea.js
@@ -3,6 +3,13 @@ import { Component, useEffect, useState } from "react";
 const TypingArea = ({ data }) => {
   const [inputData, setInputData] = useState([]);
   const [copyInputData, setCopyInputData] = useState([]);
+  const [timer, setTimer] = useState("");
+  const [intialTime, setIntilaTime] = useState("");
+  const [updateTime, setUpdateTime] = useState({
+    minutes: 0,
+    seconds: 0,
+    milliseconds: 0,
+  });
 
   const newId = () => Math.random() * 100;
 
@@ -54,14 +61,44 @@ const TypingArea = ({ data }) => {
       cd.pop();
     } else if (cd.length !== inputData.length) {
       cd.push(e.key);
+
+      if (cd.length === inputData.length) {
+        setIntilaTime("stop");
+      }
+    } else {
+      setIntilaTime("stop");
+      return;
     }
     setCopyInputData([...cd]);
     checkLetters(cd);
+    if (!intialTime && timer && intialTime !== "stop") {
+      setIntilaTime(new Date());
+    }
   };
 
   useEffect(() => {
     setInputData(handleData(data));
   }, []);
+
+  useEffect(() => {
+    let counter;
+    if (intialTime && intialTime !== "stop") {
+      counter = setInterval(() => {
+        const startDate = new Date(intialTime); // Start date
+        const endDate = new Date();
+        const startTimestamp = startDate.getTime();
+        const endTimestamp = endDate.getTime();
+        // Calculate the time difference in milliseconds
+        const timeDifference = endTimestamp - startTimestamp;
+        // Calculate the minutes and seconds
+        const minutes = Math.floor(timeDifference / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const milliseconds = timeDifference % 1000;
+        setUpdateTime({ minutes, seconds, milliseconds });
+      }, 1);
+    }
+    return () => clearInterval(counter);
+  }, [intialTime, updateTime]);
 
   useEffect(() => {
     window.addEventListener("keyup", handleTyping);
@@ -73,12 +110,45 @@ const TypingArea = ({ data }) => {
     let h = handleData(data);
     setCopyInputData([]);
     setInputData(h);
+    setIntilaTime("");
+    setUpdateTime({
+      minutes: 0,
+      seconds: 0,
+      milliseconds: 0,
+    });
   };
 
   return (
     <div className="typing-area-con">
+      {timer && (
+        <div className="timer-con">
+          <p>
+            {updateTime.minutes +
+              " : " +
+              updateTime.seconds +
+              " : " +
+              updateTime.milliseconds}
+          </p>
+        </div>
+      )}
       <div className="typing-operation">
         <button onClick={() => window.location.reload(false)}>«</button>
+        <button
+          className="timer"
+          style={timer ? { background: "#67d5b5" } : {}}
+          onClick={() => {
+            let ta = timer ? "" : "1";
+            setTimer(ta);
+            setIntilaTime("");
+            setUpdateTime({
+              minutes: 0,
+              seconds: 0,
+              milliseconds: 0,
+            });
+          }}
+        >
+          &#x1F570;
+        </button>
         {data.type === "letters" && <button onClick={refresh}>↻</button>}
       </div>
 
